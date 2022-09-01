778575ed80ee08ceb60684844fadb57c0486a14a
Caio Oliveira <102765129+Zottinho@users.noreply.github.com>
2022-09-01 13:13:38 -0300

    2022

---
+++ testes/003-repeticao/repeticao.js
@@ -0,0 +1,20 @@
+
+/*
+
+var c = 1
+console.log()
+
+while(c<=10){
+    console.log('chambra')
+    c++
+}
+
+do{
+    console.log('chambra')
+    c++
+} while(c<=2)
+*/
+
+for (var c=1; c<=8; c++){
+    console.log('chambra')
+}
