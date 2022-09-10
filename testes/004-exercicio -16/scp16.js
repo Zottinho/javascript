e27273c2cbcb10ead04f0d89a7b43c637eea2189
Caio Oliveira <102765129+Zottinho@users.noreply.github.com>
2022-09-10 14:43:40 -0300

    202209

--- testes/004-exercicio -16/scp16.js
+++ testes/004-exercicio -16/scp16.js
@@ -1,6 +1,36 @@
 function contar(){
+   
+    var cinicio = window.document.getElementById('inicio')
+    var cfim = window.document.getElementById('final')
+    var passo = window.document.getElementById('passo')
+    let res = document.getElementById("resultado")
     
 
-
+    
+    
+    if (cinicio.value.length == 0 || cfim.value.length ==0 || passo.value.length ==0 ){
+        res.innerHTML = 'Impossivel contar! Insira valores'
+    } else{
+        res.innerHTML = 'Contando:'
+        let i = Number(cinicio.value)
+        let f = Number(cfim.value)
+        let p = Number(passo.value)
+        if (p <=0){
+            window.alert('passo Inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i <f ){
+            //contagem crescente
+            for(var c = i; c <= f;c = c += p){
+                res.innerHTML += `${c}  `
+            }
+        } else{
+            for(var c = i; c >= f;c = c -= p){
+                res.innerHTML += `${c}  `
+            }
+        }
+        res.innerHTML +='FIM'
+    }
+   
 
 }
