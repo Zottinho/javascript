924fd1109d5d3fc723b495039d5016d2d5625751
Caio Oliveira <102765129+Zottinho@users.noreply.github.com>
2022-09-16 14:26:27 -0300

    202210

---
+++ testes/005-array/ex2.js
@@ -0,0 +1,14 @@
+let coisa = [5,6,6,0,20]
+/*
+num[6] = 3 --- insere mais um elemento na posição 6
+num.push(7) -- cria um elemento 7 na ultima posição
+num.length ---- comprimento
+num.sort() --- ordena os elementos
+console.log(num[0]) --- visualisa valor da array no indice 0
+
+
+
+*/
+
+coisa[10] = 13
+console.log(coisa)
